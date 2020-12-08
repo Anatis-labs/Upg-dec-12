e9aa5b29b5311c39eb10735ce022885ba070c922
Anatis <broberg.daniel89@gmail.com>
2020-12-08 16:48:54 +0100

    upload

---
+++ uppgift-dec-13/uppgift-dec-13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace uppgift_dec_13
+{
+    class Program
+    {
+        public string TextLine { get; set; }
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Hello World!");
+            var student = new Student();
+
+
+            bool loop = true;
+            while (loop == true)
+            {
+                Console.WriteLine("Pick a choice in the menu:");
+                Console.WriteLine("1: Enter a text");
+                Console.WriteLine("2: Edit entered text");
+                Console.WriteLine("3: Revert the last change");
+                Console.WriteLine("4: Write out the saved textline");
+                Console.WriteLine("5: Quit");
+
+                int SwitchChoice = Convert.ToInt32(Console.ReadLine());
+                switch (SwitchChoice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a row text:");
+                        student.Status = Console.ReadLine();
+                        student.Save(student.Status);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the new line of text");
+                        string newString = Console.ReadLine();
+                        student.Status = newString;
+                        break;
+                    case 3:
+                        student.Revert();
+                        Console.WriteLine("Your string has been reverted");
+                        break;
+                    case 4:
+                        Console.WriteLine("The textline is: {0}", student.Status);
+                        break;
+                    case 5:
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+
+}
---
+++ uppgift-dec-13/uppgift-dec-13/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uppgift_dec_13
+{
+    class Student
+    {
+        public String Status { get; set; }
+        private Memento memento;
+
+        public Student()
+        {
+            this.Status = "not right";
+            this.memento = new Memento(this.Status);
+        }
+
+        public void Save(string update)
+        {
+            this.Status = update;
+            this.memento = new Memento(this.Status);
+        }
+
+        //public void StartAdmitionProcess()
+        //{
+        //    this.Status = "Admited";
+        //}
+
+        public void Revert()
+        {
+            this.Status = this.memento.status;
+        }
+    }
+
+    class Memento
+    {
+        public readonly string status;
+
+        public Memento(String status)
+        {
+            this.status = status;
+        }
+    }
+}
+
